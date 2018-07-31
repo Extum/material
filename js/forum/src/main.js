9c9f4ab1a7a3d6892a7650656b9b4adb581e72b0
Joel Bond <joel.bond@partialdev.com>
2018-07-31 11:53:38 -0600

    Fixed a mistake with the initializer

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -2,7 +2,7 @@ import app from 'flarum/app';
 import {extend} from 'flarum/extend';
 import Page from 'flarum/components/Page';
 
-app.initializers.add('zaptech-cookie-consent', () => {
+app.initializers.add('extum/flarum-ext-material', () => {
     extend(Page.prototype, 'init', function () {
         $('head').prepend('<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">' +
             '<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>');
