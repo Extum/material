bfb26db7d17ab4450079beeb3a8685dffafdd6dc
Arda Çebi <ardacebi1@gmail.com>
2018-07-31 21:53:06 +0300

    Preparation for #109

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -16,7 +16,14 @@ System.register('extum/flarum-ext-material/main', ['flarum/app', 'flarum/extend'
 
             app.initializers.add('extum/flarum-ext-material', function () {
                 extend(Page.prototype, 'init', function () {
-                    $('head').prepend('<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">' + '<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>');
+                    $('head').prepend('<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">' + 
+                                      '<link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/styles.css">' +
+                                      '<link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/baseline.css">' +
+                                      '<link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/sharp.css">' +    
+                                      '<link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/outline.css">' +  
+                                      '<link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/round.css">' +       
+                                      '<link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/twotone.css">' + 
+                                      '<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>');
                 });
             });
         }
