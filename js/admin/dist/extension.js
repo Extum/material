ad532dde2e64b21c5de56688d7d23f743306790c
Arda Çebi <ardacebi1@gmail.com>
2018-08-01 11:23:37 +0300

    Revert to old version

--- js/admin/dist/extension.js
+++ js/admin/dist/extension.js
@@ -1,24 +1,17 @@
 'use strict';
 
-System.register('extum/flarum-ext-material/main', ['flarum/app', 'flarum/extend', 'flarum/components/Page'], function (_export, _context) {
+System.register('extum/flarum-ext-material/main', ['flarum/app'], function (_export, _context) {
   "use strict";
 
-  var app, extend, Page;
+  var app;
   return {
     setters: [function (_flarumApp) {
       app = _flarumApp.default;
-    }, function (_flarumExtend) {
-      extend = _flarumExtend.extend;
-    }, function (_flarumComponentsPage) {
-      Page = _flarumComponentsPage.default;
     }],
     execute: function () {
 
       app.initializers.add('extum/flarum-ext-material', function () {
-        extend(Page.prototype, 'init', function () {
-          $('head').prepend('<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">' +
-              '<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>');
-        });
+        //console.log('Hello, admin!');
       });
     }
   };
