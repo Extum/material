2af1ce8951c4f6cf64a7ac09601ce059a4c2b57b
Nathan Knauss <40847446+Nathan2023@users.noreply.github.com>
2018-08-23 11:52:28 -0400

    Update main.js

--- js/forum/src/main.js
+++ js/forum/src/main.js
@@ -1,8 +1,3 @@
 import app from 'flarum/app';
 
 app.initializers.add('extum/flarum-ext-material', () => {});
-
-// Using JQuery or Ajax is a must
-$(".Button-Label").each( function() {
-	$(this).text('New')
-});
