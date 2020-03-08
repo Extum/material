98d469df5afd94a49714de35648643d353c4f16a
OakleyHippo <OakleyHippo@gmail.com>
2020-03-08 13:55:54 -0400

    0.1.0-beta.12 compatibility

---
+++ extend.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of extum/flarum-ext-material.
+ *
+ * Copyright (c) 2020 Arda Cebi.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace KodedScorpius\Material;
+
+use Flarum\Extend;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/resources/less/forum.less'),
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js')
+        ->css(__DIR__.'/resources/less/admin.less'),
+    new Extend\Locales(__DIR__ . '/resources/locale')
+];
